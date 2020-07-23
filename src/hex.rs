152c57ce7df8bafe5f94b2e1bf3b2bf0eb63ea23
Jonas Bushart <jonas@bushart.org>
2020-07-23 23:44:48 +0200

    Document the hex module
    Add more tests too

--- src/hex.rs
+++ src/hex.rs
@@ -1,25 +1,62 @@
-use crate::{de::DeserializeAs, ser::SerializeAs};
-use serde::{Deserialize, Deserializer, Serializer};
+//! De/Serialization of hexadecimal encoded bytes
+//!
+//! This modules is only available when using the `hex` feature of the crate.
 
+use crate::{
+    de::DeserializeAs,
+    formats::{Format, Lowercase, Uppercase},
+    ser::SerializeAs,
+};
+use serde::{de::Error, Deserialize, Deserializer, Serializer};
+use std::{borrow::Cow, marker::PhantomData};
+
+/// Serialize bytes as a hex string
+///
+/// The type serializes a sequence of bytes as a hexadecimal string.
+/// It works on any type implementing `AsRef<[u8]>` for serialization and `From<Vec<u8>>` for deserialization.
+///
+/// The format type parameter specifies if the hex string should use lower- or uppercase characters.
+/// Valid options are the types [`Lowercase`] and [`Uppercase`].
+/// Deserialization always supports lower- and uppercase characters, even mixed in one string.
+// TODO example
 #[derive(Copy, Clone, Debug, Default)]
-pub struct Hex;
+pub struct Hex<FORMAT: Format = Lowercase>(PhantomData<FORMAT>);
 
-// TODO: AsRef
-impl SerializeAs<Vec<u8>> for Hex {
-    fn serialize_as<S>(source: &Vec<u8>, serializer: S) -> Result<S::Ok, S::Error>
+impl<T> SerializeAs<T> for Hex<Lowercase>
+where
+    T: AsRef<[u8]>,
+{
+    fn serialize_as<S>(source: &T, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        serializer.serialize_str(&::hex::encode(source))
+        serializer.serialize_str(&hex::encode(source))
     }
 }
 
-impl<'de> DeserializeAs<'de, Vec<u8>> for Hex {
-    fn deserialize_as<D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+impl<T> SerializeAs<T> for Hex<Uppercase>
+where
+    T: AsRef<[u8]>,
+{
+    fn serialize_as<S>(source: &T, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&hex::encode_upper(source))
+    }
+}
+
+impl<'de, T, FORMAT> DeserializeAs<'de, T> for Hex<FORMAT>
+where
+    T: From<Vec<u8>>,
+    FORMAT: Format,
+{
+    fn deserialize_as<D>(deserializer: D) -> Result<T, D::Error>
     where
         D: Deserializer<'de>,
     {
-        // FIXME: map decode errors
-        <&'de str as Deserialize<'de>>::deserialize(deserializer).map(|s| ::hex::decode(s).unwrap())
+        <Cow<'de, str> as Deserialize<'de>>::deserialize(deserializer)
+            .and_then(|s| hex::decode(&*s).map_err(Error::custom))
+            .map(Into::into)
     }
 }
